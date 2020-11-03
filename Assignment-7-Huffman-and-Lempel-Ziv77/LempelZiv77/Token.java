bf5c98a8bb9398fb79882713fa4d412fb7e05879
Magnus2142 <54250237+Magnus2142@users.noreply.github.com>
2020-11-03 08:59:02 +0100

    Add files via upload

---
+++ Assignment-7-Huffman-and-Lempel-Ziv77/LempelZiv77/Token.java
@@ -0,0 +1,14 @@
+package LempelZiv77;
+
+public class Token {
+
+    int offset;
+    int length;
+    int nextChar;
+
+    public Token(int offset, int length, int nextChar) {
+        this.offset = offset;
+        this.length = length;
+        this.nextChar = nextChar;
+    }
+}
